bd2b8c7f4783d244e2b8afb647530401db70d1ad
First Last <zicbo.private@gmail.com>
2014-10-18 12:54:55 +0000

    Changes made by First Last (zicbo.private@gmail.com).

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,5 +1,43 @@
-(function() {
+(function () {
     document.addEventListener("deviceready", function () {
-        var app = new kendo.mobile.Application(document.body, { skin: "flat" });
+        var everlive = new Everlive("5tdnb7FCSLwS3Siu");
+        window.listView = kendo.observable({
+            addImage: function () {
+                var success = function (data) {
+                    everlive.Files.create({
+                        Filename: Math.random().toString(36).substring(2, 15) + ".jpg",
+                        ContentType: "image/jpeg",
+                        base64: data
+                    }).then(loadPhotos);
+                };
+                var error = function () {
+                    navigator.notification.alert("Unfortunately we could not add the image");
+                };
+                var config = {
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    targetHeight: 400,
+                    targetWidth: 400
+                };
+                navigator.camera.getPicture(success, error, config);
+            }
+        });
+
+        var app = new kendo.mobile.Application(document.body, {
+            skin: "flat"
+        });
+
+        function loadPhotos() {
+            everlive.Files.get().then(function (data) {
+                var files = [];
+                data.result.forEach(function (image) {
+                    files.push(image.Uri);
+                });
+                $("#images").kendoMobileListView({
+                    dataSource: files,
+                    template: "<img src='#: data #'>"
+                });
+            });
+        }
+        loadPhotos();
     });
 }());
